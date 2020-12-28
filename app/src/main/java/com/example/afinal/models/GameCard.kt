b2dec2c360421cf80f62786c3b98b48d47bdb111
fole77 <2994245568@qq.com>
2020-12-28 09:47:36 +0800

    优化

--- app/src/main/java/com/example/afinal/models/GameCard.kt
+++ app/src/main/java/com/example/afinal/models/GameCard.kt
@@ -2,6 +2,6 @@ package com.example.afinal.models
 
 data class GameCard(
     val identifier: Int,
-    var isFaceUp: Boolean = false,   //卡片朝下
-    var isMatched: Boolean = false   //是否匹配
+    var isFaceUp: Boolean = false,
+    var isMatched: Boolean = false
 )
